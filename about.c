7f5ad25c5ee997df5feaed2cdd0b5e84976ef24f
Minh Đức <151888529+DucZaki@users.noreply.github.com>
2024-06-01 15:43:05 +0700

    Update about.c

--- about.c
+++ about.c
@@ -1,5 +1,35 @@
 #include<stdio.h>
-int main(){
-    printf("Anh đức đẹp trai vãi đái");
+void nhap_mang(int a[], int n){
+    for( int i =0; i<n; i++){
+        printf("Nhập vào giá trị của a[%d]:", i+1);
+        scanf("%d", &a[i]);
+    }
+}
+int check( int a[], int n){
+    int cout =0;
+    for (int i=0; i<n; i++){
+        int cout_2=5;
+        if( a[i]<2) cout_2 =1;
+        else{
+            for( int c=2; c<a[i];c++){
+                if(a[i]%c==0){
+                    cout_2=2;
+                    break;
+                }
+            }
+        }
+        if(cout_2==5) cout++;
+    }
+    return cout;
+}
+
+int main(void){
+    int n;
+    printf("Nhập vào số phần tử trong mảng");
+    scanf("%d", &n);
+    int a[n];
+    nhap_mang(a, n);
+    printf("trong mảng có %d số là số nguyên tố ", check(a,n));
+    printf("anh đức đẹp trai vãi đái");
     return 0;
 }
